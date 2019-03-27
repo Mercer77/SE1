3cc2eef11d45b9a4b5b00ba1d9726e253b469885
Mercer77 <1053911413@qq.com>
2019-03-27 10:34:14 +0800

    咖啡订购app

---
+++ app/src/main/java/com/example/mercer/se1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mercer.se1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.part_bottom);
+    }
+}
